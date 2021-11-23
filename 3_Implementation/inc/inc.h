1d0e15b61c2cf72114856f635e84a130c3d77a29
AdithyaBK <88897081+AdithyaBK@users.noreply.github.com>
2021-11-23 15:48:51 +0530

    Update inc.h

--- 3_Implementation/inc/inc.h
+++ 3_Implementation/inc/inc.h
@@ -1,3 +1,14 @@
+/**
+ * @SnakeAndLadder.h
+ * @author your name (Adithyabk)
+ * @brief 
+ * @version 0.1
+ * @date 2021-11-23
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
 #include<stdio.h>
 #include<stdlib.h>
 int rd,rem;
